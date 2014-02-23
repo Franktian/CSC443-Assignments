80b2e7f29d5ed2dd31bf0dd1cb8bdd6185aa8187
Eric Zhu <erkangzhu@gmail.com>
2014-02-23 14:50:48 -0500

    start merge runs

--- pl2/library.h
+++ pl2/library.h
@@ -12,3 +12,22 @@ typedef unsigned long Offset;
  * The length of the run must be a multiple of 9
  */
 void mk_runs(FILE *in_fp, FILE *out_fp, Offset run_length);
+
+class RunIterator {
+    /**
+     * creates an iterator using the `buf_size` to
+     * scan through a run that starts at `start_pos`
+     * with length `run_length`
+     */
+    RunIterator(FILE *fp, long start_pos, long run_length, long buf_size);
+    /**
+     * reads the next record.  Returns null if
+     * iterator reads the end of the run.
+     */
+    Record next();
+};
+
+void merge_runs(FILE *out_fp, 
+                RunIterator iterators[], 
+                int num_iterators, 
+                long buf_size);
