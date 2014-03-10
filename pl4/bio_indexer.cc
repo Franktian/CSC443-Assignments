9f93ebc5c82578669d35b012f8420deb4a25f9eb
Yawen Tian <tianyawen201209@hotmail.com>
2014-03-10 18:30:46 -0400

    Add starter code

---
+++ pl4/bio_indexer.cc
@@ -0,0 +1,122 @@
+#include <xapian.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <sys/timeb.h>
+ 
+using namespace std;
+
+int main(int argc, char **argv) {
+
+
+    if (argc != 3) {
+		cout << "ERROR: invalid input parameters!" << endl;
+		cout << "Usage: <parsed_biographies> <index_name>" << endl;
+		exit(1);
+	}
+
+	ifstream file;
+
+	file.open(argv[1]);
+
+	if (!file) {
+		cout << "Read file failed" << endl;
+		exit(0);
+	}
+
+	string line;
+
+	char *dbname = argv[2];
+
+
+
+
+	try {
+		Xapian::WritableDatabase db(dbname, Xapian::DB_CREATE_OR_OPEN);
+		Xapian::Document doc;
+
+		// Record Start Time 
+	    cout << "Start the timer" << endl;
+	    struct timeb _t;
+	    long total = 0;
+	    ftime(&_t);
+	    long start = _t.time * 1000 + _t.millitm;
+	    
+
+
+		while (getline(file, line)) {
+			// First line must be a person's name, copy it
+			char *name = new char [line.size() + 1];
+			std::copy(line.begin(), line.end(), name);
+			name[line.size()] = '\0';
+
+			// cast name to a string
+			string person_name(name, line.size() + 1);
+
+			// The line following it is the biography, load it
+
+			getline(file, line);
+
+
+
+			istringstream iss(line);
+
+			// Tokenize the biography
+
+			vector<string> tokens;
+			copy(istream_iterator<string>(iss),
+	         istream_iterator<string>(),
+	         back_inserter<vector<string> >(tokens));
+
+
+
+			// This is to check if term already exists
+			vector<string> terms;
+
+
+			// Add terms to this person
+			for (vector<string>::iterator it = tokens.begin() ; it != tokens.end(); ++it) {
+				if (find(terms.begin(), terms.end(), *it) == terms.end()) {
+					// If we do not have that term, we insert it
+					doc.add_term(*it);
+					terms.push_back(*it);
+				}
+			}
+
+			// Set value and data
+			doc.add_value(0, person_name);
+			doc.set_data(line);
+			db.add_document(doc);
+			
+
+			
+
+			// Clear the document ready for the next
+			doc.clear_terms();
+			doc.clear_values();
+
+		}
+
+		db.commit();
+		ftime(&_t);
+		long finish = _t.time * 1000 + _t.millitm;
+		total = finish - start;
+
+		cout << "TIME : " << total << " milliseconds" << endl;
+
+	    
+	} catch (const Xapian::Error & error) {
+		cout << "Exception: " << error.get_msg() << endl;
+	}
+
+	
+
+	file.close();
+
+	return 0;
+
+}
---
+++ pl4/bio_search.cc
@@ -0,0 +1,129 @@
+#include <xapian.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sys/timeb.h>
+#include <cctype>
+#include <cstdio>
+
+using namespace std;
+
+void replace(string& str, const string& from, const string& to) {
+    if(from.empty()) { return; }
+    size_t start_pos = 0;
+    size_t to_length = to.length();
+	size_t from_length = from.length();
+    while((start_pos = str.find(from, start_pos)) != string::npos) {
+    	// Check to make sure the term is not a part of another word
+		if (start_pos+from_length < str.length() && 
+			str.at(start_pos+from_length) != ' ') {
+			start_pos += from_length;
+			continue;
+		}
+		if (start_pos > 0 && str.at(start_pos-1) != ' ') {
+			start_pos += from_length;
+			continue;
+		}
+        str.replace(start_pos, from_length, to);
+        start_pos += to_length;
+    }
+}
+
+int main(int argc, char **argv) {
+	if (argc < 4) {
+        cout << "ERROR: invalid input parameters!" << endl;
+        cout << "bio_search <index_name> <top-k> <keyword1> <keyword2> ..." << endl;
+        exit(1);
+    }
+    char* index_name = (char*)argv[1];
+    int k = atoi((argv[2]));
+    int num_search_terms = argc - 3;
+    if (k < 1) {
+        cout << "top-k must be at least 1!" << endl;
+        exit(1);
+    }
+
+    // Start timer
+    cout << "Start the timer" << endl;
+    struct timeb _t;
+    ftime(&_t);
+    long start = _t.time * 1000 + _t.millitm;
+    
+    try {
+	    // Open the database
+	    Xapian::Database db(index_name);
+	    
+	    // Construct the query terms and query string
+	    vector<string> query_terms;
+	    string query_string;
+	    for (int i = 3; i < argc; ++i) {
+	    	char* term = argv[i];
+		// transform the search terms to lower case
+		char c; int j = 0;
+		while (term[j]) {
+			term[j] = tolower(term[j]);
+			j++;
+		}
+	    	if (term[0] == '+') {
+	    		query_terms.push_back(term+1);
+	    	} else {
+	    		query_terms.push_back(term);
+	    	}
+	    	query_string.append(term).append(" ");
+	    }
+
+	    Xapian::QueryParser parser;
+	    Xapian::Query query = parser.parse_query(query_string);
+
+	    // Pass in operator and vector iterators
+		// Xapian::Query query(Xapian::Query::OP_OR, query_terms.begin(), query_terms.end());
+		cout << "Performing query `" << query.get_description() << "`" << endl;
+
+		// Run the query
+		Xapian::Enquire enquire(db);
+		enquire.set_query(query);
+		Xapian::MSet matches = enquire.get_mset(0, k);
+		cout << "Result mset size is " << matches.size() << endl;
+
+		// Prepare the highlighted search terms
+		vector<string> highlighted_terms;
+		for (vector<string>::iterator it = query_terms.begin(); 
+				it != query_terms.end(); ++it)
+		{
+			string highlight = "====" + (*it) + "====";
+			highlighted_terms.push_back(highlight);
+		}
+
+		// Show the result
+		Xapian::MSetIterator i;
+		for (i = matches.begin(); i != matches.end(); ++i) {
+			// Print the document ID and matching metrics
+			cout << "Document ID " << *i << "\t";
+			cout << i.get_percent() << "% \t";
+			Xapian::Document doc = i.get_document();
+
+			// Print the person name
+			cout << doc.get_value(0) << endl;
+
+			// Get the original document and highlight the search terms
+			string data = doc.get_data();
+			//cout << data << endl;
+			for (int j = 0; j < num_search_terms; j++) {
+				replace(data, query_terms.at(j), highlighted_terms.at(j));
+			}
+
+			// Print the highlighted document
+			cout << "[" << data << "]" << endl;
+		}
+
+    } catch (const Xapian::Error & error) {
+		cout << "Exception: " << error.get_msg() << endl;
+	}
+
+    // Stop the timer
+    cout << "Stop the timer" << endl;
+    ftime(&_t);
+    long finish = _t.time * 1000 + _t.millitm;
+    cout << "TIME : " << finish - start << " milliseconds" << endl;
+	return 0;
+}
