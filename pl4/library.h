335b8451495568f11e907990c31b766528dfd683
Yawen Tian <tianyawen201209@hotmail.com>
2014-03-18 21:46:41 -0400

    Completed headers

--- pl4/library.h
+++ pl4/library.h
@@ -1,4 +1,6 @@
 
 using namespace std;
+string encrpytion (string to_be_encrpyted);
+vector<string> tokenizer (string to_be_tokenized, int n);
 vector<string> ngram_tokenizer (string to_be_tokenized, int n);
 vector<string> space_tokenizer (string to_be_tokenized);
